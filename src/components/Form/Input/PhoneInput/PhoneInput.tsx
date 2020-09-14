73ea394cd96430ff198d5ef948da4e195209bbb2
Amanda Barboza <amanda.barboza@mirumagency.com>
2020-09-13 21:18:20 -0300

    tenta validacao select

--- src/components/Form/Input/PhoneInput/PhoneInput.tsx
+++ src/components/Form/Input/PhoneInput/PhoneInput.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component, FormEvent } from 'react';
 import './PhoneInput.css';
 import IntlTelInput from 'react-intl-tel-input';
+export interface PhoneProps {
+  id: string;
+}
+export interface PhoneState {
+  value: any;
+  phone: any;
+}
+class PhoneInput extends Component<PhoneProps, PhoneState> {
+  constructor(props: PhoneProps) {
+    super(props);
+    this.state = {
+      phone: '',
+      value: '',
+    };
+    this.changePhone = this.changePhone.bind(this);
+  }
 
-const PhoneInput = (props: { id: string }) => {
-  return (
-    <IntlTelInput id={props.id} style={{ width: "100%" }}
-      preferredCountries={['br']}
-    />
-  )
+  public changePhone(event: FormEvent<HTMLInputElement>): void {
+    let phone = event.currentTarget.value;
+    const re = /^[0-9\b]+$/;
+
+    if (phone === '' || re.test(phone)) {
+      this.setState({ value: phone })
+    }
+  }
+
+  public render(): JSX.Element {
+    return (
+      <IntlTelInput id={this.props.id} preferredCountries={['br']} style={{ width: "100%" }}  onChange={this.changePhone} />
+    );
+  }
 }
 
 export default PhoneInput;
